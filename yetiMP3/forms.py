84ec51d5d557b0fc3e98e728c5703eb436f61ff1
christian.celora@gmail.com <christian.celora@gmail.com>
2020-11-21 19:41:49 +0100

    Added bootstrap 4 - jquery. Styling index

--- yetiMP3/forms.py
+++ yetiMP3/forms.py
@@ -2,10 +2,11 @@ from django import forms
 
 class YTForm(forms.Form):
     url = forms.CharField()
+    auto_id = False
 
     def __init__(self, *args, **kwargs):
         super(YTForm, self).__init__(*args, **kwargs)
-        self.fields['url'].widget.attrs.update({'id':'yt_url'})
+        self.fields['url'].widget.attrs.update({'id':'yt_url', 'class':'form-text'})
 
     #def send_email(self):
         # send email using the self.cleaned_data dictionary
--- yetiMP3/settings.py
+++ yetiMP3/settings.py
@@ -121,3 +121,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (os.path.join(os.path.dirname(__file__), 'static/'),)
