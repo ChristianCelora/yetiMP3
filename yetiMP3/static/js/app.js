11fb90b9fb46f1761294636ddbe8d5008fb752ad
christian.celora@gmail.com <christian.celora@gmail.com>
2020-12-11 21:58:41 +0100

    fix form submit

--- yetiMP3/static/js/app.js
+++ yetiMP3/static/js/app.js
@@ -1,6 +1,9 @@
-console.log("yeti mp3");
+$(document).ready(function(){
+    console.log("yeti mp3");
 
-$("#yt-form-submit").click(function(){
-    console.log("form submit")
-    $("#yt-form").submit()
-})
+    $("#yt-form-submit").click(function(){
+        console.log("form submit")
+        // should be AJAX call 
+        $("#yt-form").submit()
+    })
+});
