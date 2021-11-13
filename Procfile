16d75fbfcb531553028b99da05a236053b37ec08
christian.celora@gmail.com <christian.celora@gmail.com>
2021-11-13 15:28:42 +0100

    Updated procfile db migrations

--- Procfile
+++ Procfile
@@ -1,2 +1,3 @@
 web: gunicorn yetiMP3.wsgi --timeout 15 --log-file -
 worker: celery -A yetiMP3 worker -B --loglevel=info
+release: python manage.py migrate
--- yetiMP3/tasks.py
+++ yetiMP3/tasks.py
@@ -14,7 +14,8 @@ def download_yt_mp3(url, new_name, task_id):
     real_name, client_name =  ytd.download(url, new_name)
     #update db
     task = Task.objects.get(id=task_id)
-    task.status = 1
-    task.file_name = real_name
-    task.frontend_name = client_name
-    task.save()
+    if(task):
+        task.status = 1
+        task.file_name = real_name
+        task.frontend_name = client_name
+        task.save()
